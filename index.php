b6f90c9e2278bc9890ebc07340cdebcafb0d0023
yuji-0517mine <159821681+yuji-0517mine@users.noreply.github.com>
2024-02-13 16:31:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $error_message = "";
 
 if(isset($_POST["login"])) {
 
-	if($_POST["user_name"] == "webtan" && $_POST["password"] == "webtan_pass") {
+	if($_POST["user_name"] == "Kanure0X" && $_POST["password"] == "Yuji7720") {
 		$_SESSION["user_name"] = $_POST["user_name"];
 		$login_success_url = "login_success.php";
 		header("Location: {$login_success_url}");
